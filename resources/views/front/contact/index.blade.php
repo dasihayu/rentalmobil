13f9a18e092134bf50d9fe5ad7440b5eab861c5a
dasihayu <dasihayu1307@gmail.com>
2025-01-27 20:05:48 +0700

    feat: make contact page

---
+++ resources/views/front/contact/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.front')
+@section('title', 'Contact Us')
+@section('content')
+@include('front.rental.sections.banner')
+@include('front.rental.sections.contact')
+@endsection
---
+++ resources/views/front/contact/sections/contact.blade.php
@@ -0,0 +1,60 @@
+<section class="files my-80">
+    <div class="container-fluid">
+        <div class="row-gap-4 row align-items-end">
+            <div class="my-40 col-lg-6 col-md-6 col-sm-6">
+                <form>
+                    <div class="topic">
+                        <h6 class="mb-12">Select a topic</h6>
+                        <div class="flex-wrap gap-24 d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="Signing" name="topic" class="d-none" checked>
+                                <label for="Signing"> Signing in</label>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="booking" name="topic" class="d-none">
+                                <label for="booking"> Booking issue</label>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="report" name="topic" class="d-none">
+                                <label for="report"> Report a correction</label>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="work" name="topic" class="d-none">
+                                <label for="work"> Booking issue</label>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="us" name="topic" class="d-none">
+                                <label for="us">Work with us</label>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="in" name="topic" class="d-none">
+                                <label for="in"> Signing in</label>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="Works" name="topic" class="d-none">
+                                <label for="Works">Work with us</label>
+                            </div>
+                            <div class="flex-shrink-0">
+                                <input type="radio" id="something" name="topic" class="d-none">
+                                <label for="something"> Need something else</label>
+                            </div>
+                        </div>
+                        <h6 class="mt-24 mb-24">Or tell us what you need help with:</h6>
+                        <input type="text" placeholder="Your Name *">
+                        <a href="contact.html" class="mt-24 cus-btn">
+                            <span class="btn-text">
+                                Get Help
+                            </span>
+                            <span>
+                                Get Help
+                            </span>
+                        </a>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <img src="{{ asset('front/assets/media/blogs/pngwing-19.png') }}" alt="car">
+            </div>
+        </div>
+    </div>
+</section>
